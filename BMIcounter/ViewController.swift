6dafb16e10518ebfd140c0e2265139426871947b
yangisee <yvincent74@gmail.com>
2016-01-24 12:38:29 -0500

    Initial Commit

---
+++ BMIcounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMIcounter
+//
+//  Created by YangHuairuo on 2016-01-24.
+//  Copyright © 2016 Yangisee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
